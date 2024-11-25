3c8602eb64cb92eeffd471cccc43957cd7c11872
Shah Me'raj <109169939+ShahMehraj@users.noreply.github.com>
2024-11-25 11:20:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -68,7 +68,7 @@ def user_input(user_question):
 
 def main():
     st.set_page_config("Chat PDF")
-    st.header("Extract financial information from Annual Reports 💬")
+    st.header("Annual Reports Parser 💬")
 
     user_question = st.text_input("Ask a Question from the PDF Files")
 
