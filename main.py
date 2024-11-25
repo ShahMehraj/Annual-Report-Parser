a65fe65c7e4134c74d1da457a796406bab888dbd
Shah Me'raj <109169939+ShahMehraj@users.noreply.github.com>
2024-11-25 11:11:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -68,7 +68,7 @@ def user_input(user_question):
 
 def main():
     st.set_page_config("Chat PDF")
-    st.header("Chat with multiple PDF using Gemini💁")
+    st.header("Annual Report Parser")
 
     user_question = st.text_input("Ask a Question from the PDF Files")
 
